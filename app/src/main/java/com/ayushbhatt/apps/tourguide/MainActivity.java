06b335912d2172c65620d26bc40243a2a923d36e
akbhatt <38679623+akbhatt@users.noreply.github.com>
2018-07-21 19:51:03 -0400

    Update Removed TextViews, Added History

--- app/src/main/java/com/ayushbhatt/apps/tourguide/MainActivity.java
+++ app/src/main/java/com/ayushbhatt/apps/tourguide/MainActivity.java
@@ -1,19 +1,21 @@
 package com.ayushbhatt.apps.tourguide;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = findViewById(R.id.tourViewPager);
+        viewPager = findViewById(R.id.tourViewPager);
         PagerAdapter tourFragmentAdapter = new TourFragmentAdapter(getSupportFragmentManager());
         viewPager.setAdapter(tourFragmentAdapter);
 
--- app/src/main/java/com/ayushbhatt/apps/tourguide/WelcomeFragment.java
+++ app/src/main/java/com/ayushbhatt/apps/tourguide/WelcomeFragment.java
@@ -7,11 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class WelcomeFragment extends Fragment {
-
 
     public WelcomeFragment() {
         // Required empty public constructor
@@ -22,18 +20,11 @@ public class WelcomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        final View welcomeView = inflater.inflate(R.layout.fragment_welcome, container, false);
+        View welcomeView = inflater.inflate(R.layout.fragment_welcome, container, false);
 
-
-        TextView toAttractions = welcomeView.findViewById(R.id.toAttractionsFragment);
-        toAttractions.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "This Works!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        TextView history = welcomeView.findViewById(R.id.detroitHistory);
+        history.setText(R.string.history_text);
         return welcomeView;
     }
-    }
+}
 
